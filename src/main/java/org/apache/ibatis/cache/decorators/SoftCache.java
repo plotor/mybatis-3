8678b0d551ddd473cfe389195eb0ae9ff0a7ba78
wangzhenchao <wangzhenchao@xiaomi.com>
2017-09-21 16:27:02 +0800

    阅读 cache 的基本组件

--- src/main/java/org/apache/ibatis/cache/decorators/SoftCache.java
+++ src/main/java/org/apache/ibatis/cache/decorators/SoftCache.java
@@ -1,27 +1,28 @@
 /**
- *    Copyright 2009-2015 the original author or authors.
+ * Copyright 2009-2015 the original author or authors.
  *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *       http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
+
 package org.apache.ibatis.cache.decorators;
+
+import org.apache.ibatis.cache.Cache;
 
 import java.lang.ref.ReferenceQueue;
 import java.lang.ref.SoftReference;
 import java.util.Deque;
 import java.util.LinkedList;
 import java.util.concurrent.locks.ReadWriteLock;
-
-import org.apache.ibatis.cache.Cache;
 
 /**
  * Soft Reference cache decorator
@@ -30,96 +31,112 @@ import org.apache.ibatis.cache.Cache;
  * @author Clinton Begin
  */
 public class SoftCache implements Cache {
-  private final Deque<Object> hardLinksToAvoidGarbageCollection;
-  private final ReferenceQueue<Object> queueOfGarbageCollectedEntries;
-  private final Cache delegate;
-  private int numberOfHardLinks;
 
-  public SoftCache(Cache delegate) {
-    this.delegate = delegate;
-    this.numberOfHardLinks = 256;
-    this.hardLinksToAvoidGarbageCollection = new LinkedList<Object>();
-    this.queueOfGarbageCollectedEntries = new ReferenceQueue<Object>();
-  }
+    /** 强引用队列，最近使用的一部分缓存会被记录到其中，不会被 GC */
+    private final Deque<Object> hardLinksToAvoidGarbageCollection;
 
-  @Override
-  public String getId() {
-    return delegate.getId();
-  }
+    /** 弱引用队列，用于记录已经被 GC 回收的缓存项锁对应的 {@link SoftEntry} 对象 */
+    private final ReferenceQueue<Object> queueOfGarbageCollectedEntries;
 
-  @Override
-  public int getSize() {
-    removeGarbageCollectedItems();
-    return delegate.getSize();
-  }
+    /** 被装饰的 {@link Cache} 对象 */
+    private final Cache delegate;
 
+    /** 强引用的个数，默认是 256 个 */
+    private int numberOfHardLinks;
 
-  public void setSize(int size) {
-    this.numberOfHardLinks = size;
-  }
+    public SoftCache(Cache delegate) {
+        this.delegate = delegate;
+        this.numberOfHardLinks = 256;
+        this.hardLinksToAvoidGarbageCollection = new LinkedList<Object>();
+        this.queueOfGarbageCollectedEntries = new ReferenceQueue<Object>();
+    }
 
-  @Override
-  public void putObject(Object key, Object value) {
-    removeGarbageCollectedItems();
-    delegate.putObject(key, new SoftEntry(key, value, queueOfGarbageCollectedEntries));
-  }
+    @Override
+    public String getId() {
+        return delegate.getId();
+    }
 
-  @Override
-  public Object getObject(Object key) {
-    Object result = null;
-    @SuppressWarnings("unchecked") // assumed delegate cache is totally managed by this cache
-    SoftReference<Object> softReference = (SoftReference<Object>) delegate.getObject(key);
-    if (softReference != null) {
-      result = softReference.get();
-      if (result == null) {
-        delegate.removeObject(key);
-      } else {
-        // See #586 (and #335) modifications need more than a read lock 
-        synchronized (hardLinksToAvoidGarbageCollection) {
-          hardLinksToAvoidGarbageCollection.addFirst(result);
-          if (hardLinksToAvoidGarbageCollection.size() > numberOfHardLinks) {
-            hardLinksToAvoidGarbageCollection.removeLast();
-          }
+    @Override
+    public int getSize() {
+        // 清除已经被 GC 的缓存项
+        this.removeGarbageCollectedItems();
+        return delegate.getSize();
+    }
+
+    public void setSize(int size) {
+        this.numberOfHardLinks = size;
+    }
+
+    @Override
+    public void putObject(Object key, Object value) {
+        // 清除已经被 GC 的缓存项
+        this.removeGarbageCollectedItems();
+        delegate.putObject(key, new SoftEntry(key, value, queueOfGarbageCollectedEntries));
+    }
+
+    @Override
+    public Object getObject(Object key) {
+        Object result = null;
+        @SuppressWarnings("unchecked") // assumed delegate cache is totally managed by this cache
+                SoftReference<Object> softReference = (SoftReference<Object>) delegate.getObject(key);
+        if (softReference != null) {
+            result = softReference.get();
+            if (result == null) {
+                delegate.removeObject(key);
+            } else {
+                // See #586 (and #335) modifications need more than a read lock
+                synchronized (hardLinksToAvoidGarbageCollection) {
+                    hardLinksToAvoidGarbageCollection.addFirst(result);
+                    if (hardLinksToAvoidGarbageCollection.size() > numberOfHardLinks) {
+                        hardLinksToAvoidGarbageCollection.removeLast();
+                    }
+                }
+            }
         }
-      }
+        return result;
     }
-    return result;
-  }
 
-  @Override
-  public Object removeObject(Object key) {
-    removeGarbageCollectedItems();
-    return delegate.removeObject(key);
-  }
-
-  @Override
-  public void clear() {
-    synchronized (hardLinksToAvoidGarbageCollection) {
-      hardLinksToAvoidGarbageCollection.clear();
+    @Override
+    public Object removeObject(Object key) {
+        // 清除已经被 GC 的缓存项
+        this.removeGarbageCollectedItems();
+        return delegate.removeObject(key);
     }
-    removeGarbageCollectedItems();
-    delegate.clear();
-  }
 
-  @Override
-  public ReadWriteLock getReadWriteLock() {
-    return null;
-  }
-
-  private void removeGarbageCollectedItems() {
-    SoftEntry sv;
-    while ((sv = (SoftEntry) queueOfGarbageCollectedEntries.poll()) != null) {
-      delegate.removeObject(sv.key);
+    @Override
+    public void clear() {
+        synchronized (hardLinksToAvoidGarbageCollection) {
+            hardLinksToAvoidGarbageCollection.clear();
+        }
+        removeGarbageCollectedItems();
+        delegate.clear();
     }
-  }
 
-  private static class SoftEntry extends SoftReference<Object> {
-    private final Object key;
-
-    SoftEntry(Object key, Object value, ReferenceQueue<Object> garbageCollectionQueue) {
-      super(value, garbageCollectionQueue);
-      this.key = key;
+    @Override
+    public ReadWriteLock getReadWriteLock() {
+        return null;
     }
-  }
+
+    /**
+     * 清除已经被 GC 的缓存项
+     */
+    private void removeGarbageCollectedItems() {
+        SoftEntry sv;
+        while ((sv = (SoftEntry) queueOfGarbageCollectedEntries.poll()) != null) {
+            delegate.removeObject(sv.key);
+        }
+    }
+
+    /**
+     * 缓存 value 实现类，继承了 {@link SoftReference}
+     */
+    private static class SoftEntry extends SoftReference<Object> {
+        private final Object key;
+
+        SoftEntry(Object key, Object value, ReferenceQueue<Object> garbageCollectionQueue) {
+            super(value, garbageCollectionQueue);
+            this.key = key; // 指向 key 的引用是强引用
+        }
+    }
 
 }
--- src/main/java/org/apache/ibatis/cache/decorators/SynchronizedCache.java
+++ src/main/java/org/apache/ibatis/cache/decorators/SynchronizedCache.java
@@ -1,78 +1,82 @@
 /**
- *    Copyright 2009-2017 the original author or authors.
+ * Copyright 2009-2017 the original author or authors.
  *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *       http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
+
 package org.apache.ibatis.cache.decorators;
 
-import java.util.concurrent.locks.ReadWriteLock;
-
 import org.apache.ibatis.cache.Cache;
+
+import java.util.concurrent.locks.ReadWriteLock;
 
 /**
  * @author Clinton Begin
  */
 public class SynchronizedCache implements Cache {
 
-  private final Cache delegate;
-  
-  public SynchronizedCache(Cache delegate) {
-    this.delegate = delegate;
-  }
+    /** 基于 {@link synchronized } 同步策略的缓存装饰器*/
 
-  @Override
-  public String getId() {
-    return delegate.getId();
-  }
+    /** 被装饰的 {@link Cache} 对象 */
+    private final Cache delegate;
 
-  @Override
-  public synchronized int getSize() {
-    return delegate.getSize();
-  }
+    public SynchronizedCache(Cache delegate) {
+        this.delegate = delegate;
+    }
 
-  @Override
-  public synchronized void putObject(Object key, Object object) {
-    delegate.putObject(key, object);
-  }
+    @Override
+    public String getId() {
+        return delegate.getId();
+    }
 
-  @Override
-  public synchronized Object getObject(Object key) {
-    return delegate.getObject(key);
-  }
+    @Override
+    public synchronized int getSize() {
+        return delegate.getSize();
+    }
 
-  @Override
-  public synchronized Object removeObject(Object key) {
-    return delegate.removeObject(key);
-  }
+    @Override
+    public synchronized void putObject(Object key, Object object) {
+        delegate.putObject(key, object);
+    }
 
-  @Override
-  public synchronized void clear() {
-    delegate.clear();
-  }
+    @Override
+    public synchronized Object getObject(Object key) {
+        return delegate.getObject(key);
+    }
 
-  @Override
-  public int hashCode() {
-    return delegate.hashCode();
-  }
+    @Override
+    public synchronized Object removeObject(Object key) {
+        return delegate.removeObject(key);
+    }
 
-  @Override
-  public boolean equals(Object obj) {
-    return delegate.equals(obj);
-  }
+    @Override
+    public synchronized void clear() {
+        delegate.clear();
+    }
 
-  @Override
-  public ReadWriteLock getReadWriteLock() {
-    return null;
-  }
+    @Override
+    public int hashCode() {
+        return delegate.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return delegate.equals(obj);
+    }
+
+    @Override
+    public ReadWriteLock getReadWriteLock() {
+        return null;
+    }
 
 }
